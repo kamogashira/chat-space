666c5a4779c5eaabed25d46f5124a646181ee46d
kamogashira <asuka.kamo@icloud.com>
2019-07-30 14:37:31 +0900

    Revert "update_message.js"
    This reverts commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,23 +1,4 @@
 $(function(){
-  function buildHTML(message){
-    var content = message.content ? `${ message.content }` : "";
-    var image = message.image ? `<img class="message__lower__image" src= ${ message.image }>` : "";
-    var html = `<div class="message__upper">
-                  <p class="message__upper__name">
-                    ${message.user_name}
-                  </p>
-                  <p class="message__upper__date">
-                    ${message.date}
-                  </p>
-                </div>
-                <div class="message__lower">
-                  <p class="message__lower__content">
-                    ${content}
-                  </p>
-                  ${image}
-                </div>`
-  return html;
-  }
   $('#box').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -29,19 +10,6 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html)
-      $('.box__input__text').val('')
-    })
-    .fail(function(){
-      alert('error');
-    })
-    .always(function(data){
-      // var result = $('.box__btn').is(':disabled');
-      // console.log(result)
-      $('.box__btn').prop('disabled', false);　//ここで解除している
     })
   })
 })
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,4 @@
-json.id          @message.id
 json.user_name   @message.user.name
-json.date        @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.created_at  @message.created_at
 json.content     @message.content
-json.image       @message.image.url
+json.image       @message.image
