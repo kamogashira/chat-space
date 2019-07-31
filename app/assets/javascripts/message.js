9e83b665f77ab56c00268038c4458da86f7ba295
kamogashira <asuka.kamo@icloud.com>
2019-07-31 10:41:56 +0900

    Update message.js
    ・ function buildHTML(message)内のhtml作成部分＝var htmlに一番上に<div class="message">を追加
    ＊理由：CSSがうまく当たっていなかったため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,26 +2,29 @@ $(function(){
   function buildHTML(message){
     var content = message.content ? `${ message.content }` : "";
     var image = message.image ? `<img class="message__lower__image" src= ${ message.image }>` : "";
-    var html = `<div class="message__upper">
-                  <p class="message__upper__name">
-                    ${message.user_name}
-                  </p>
-                  <p class="message__upper__date">
-                    ${message.date}
-                  </p>
-                </div>
-                <div class="message__lower">
-                  <p class="message__lower__content">
-                    ${content}
-                  </p>
-                  ${image}
+    var html = `<div class="message">
+                  <div class="message__upper">
+                    <p class="message__upper__name">
+                      ${message.user_name}
+                    </p>
+                    <p class="message__upper__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <div class="message__lower">
+                    <p class="message__lower__content">
+                      ${content}
+                    </p>
+                    ${image}
+                  </div>
                 </div>`
   return html;
   }
 
   function scrollBottom(){
     var target = $('.message').last();
-    var position = target.offset().top + $('.messages').scrollTop();
+    // var position = target.offset().top + $('.messages').scrollTop();
+    var position = target.offset().top;
     $('.messages').animate({
       scrollTop: position
     }, 300, 'swing');
