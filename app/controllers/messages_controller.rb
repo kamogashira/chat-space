89a8f81f2944cfab8ac04e5913d65af2cbc1a413
kamogashira <asuka.kamo@icloud.com>
2019-08-08 16:27:09 +0900

    update3
    ・インクリメンタルサーチ機能の修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,11 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    # @messages = @group.messages.includes(:user).where('id > ?', params[:id])
+    # respond_to do |format|
+    #   format.html
+    #   format.json
+    # end
   end
 
   def create
